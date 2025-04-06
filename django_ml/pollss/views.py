d2c113da097890c33bf1c343c222cac557872ce3
Kosuru23 <jusonneiljam@gmaill.com>
2025-04-07 06:48:33 +0800

    assignment ui

--- django_ml/pollss/views.py
+++ django_ml/pollss/views.py
@@ -1,6 +1,4 @@
-# polss/views.py
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import PredictionInput
 import joblib
 import os
@@ -11,85 +9,108 @@ model = joblib.load(model_path)
 def index_views(request):
     return render(request, 'index.html')
 
+# def save_index(request):
+#     prediction = None
+
+#     if request.method == 'POST':
+#         income = float(request.POST.get('income'))
+#         children = int(request.POST.get('children'))
+#         home_owner = bool(int(request.POST.get('home_owner')))
+#         cars = int(request.POST.get('cars'))
+#         age = int(request.POST.get('age'))
+#         occupation = request.POST.get('occupation')
+#         education = request.POST.get('education')
+#         marital_status = bool(int(request.POST.get('marital_status')))
+#         gender = bool(int(request.POST.get('gender')))
+#         commute_lower = float(request.POST.get('commute_lower'))
+#         commute_upper = float(request.POST.get('commute_upper'))
+#         region = request.POST.get('region')
+
+#         user_input = PredictionInput.objects.create(
+#             income=income,
+#             children=children,
+#             home_owner=home_owner,
+#             cars=cars,
+#             age=age,
+#             occupation=occupation,
+#             education=education,
+#             marital_status=marital_status,
+#             gender=gender,
+#             commute_lower=commute_lower,
+#             commute_upper=commute_upper,
+#             region=region
+#         )
+
+#         input_vector = convert_to_model_input(user_input)
+#         prediction = model.predict([input_vector])[0]
+
+#     return render(request, 'index.html', {'prediction': prediction})
+
+# def convert_to_model_input(data):
+#     occupation_map = {
+#         'Clerical': [1, 0, 0, 0, 0],
+#         'Management': [0, 1, 0, 0, 0],
+#         'Manual': [0, 0, 1, 0, 0],
+#         'Professional': [0, 0, 0, 1, 0],
+#         'Skilled Manual': [0, 0, 0, 0, 1],
+#     }
+
+#     education_map = {
+#         'Bachelors': [1, 0, 0, 0, 0],
+#         'Graduate Degree': [0, 1, 0, 0, 0],
+#         'High School': [0, 0, 1, 0, 0],
+#         'Partial College': [0, 0, 0, 1, 0],
+#         'Partial High School': [0, 0, 0, 0, 1],
+#     }
+
+#     region_map = {
+#         'Europe': [1, 0, 0],
+#         'North America': [0, 1, 0],
+#         'Pacific': [0, 0, 1],
+#     }
+
+#     base_features = [
+#         data.income,
+#         data.children,
+#         int(data.home_owner),
+#         data.cars,
+#         data.age,
+#         int(data.marital_status),
+#         int(data.gender),
+#         data.commute_lower,
+#         data.commute_upper,
+#     ]
+
+#     return base_features + occupation_map[data.occupation] + education_map[data.education] + region_map[data.region]
+
 def save_index(request):
-    prediction = None
+    income = float(request.POST['income'])
+    children = int(request.POST['children'])
+    home_owner = bool(int(request.POST['home_owner']))
+    cars = int(request.POST['cars'])
+    age = int(request.POST['age'])
+    occupation = request.POST['occupation']
+    education = request.POST['education']
+    marital_status = int(request.POST['marital_status'])
+    gender = int(request.POST['gender'])
+    commute_lower = int(request.POST['commute_lower'])
+    commute_upper = int(request.POST['commute_upper'])
+    region = request.POST['region']
 
-    if request.method == 'POST':
-        income = float(request.POST.get('income'))
-        children = int(request.POST.get('children'))
-        home_owner = bool(int(request.POST.get('home_owner')))
-        cars = int(request.POST.get('cars'))
-        age = int(request.POST.get('age'))
-        occupation = request.POST.get('occupation')
-        education = request.POST.get('education')
-        marital_status = bool(int(request.POST.get('marital_status')))
-        gender = bool(int(request.POST.get('gender')))
-        commute_lower = float(request.POST.get('commute_lower'))
-        commute_upper = float(request.POST.get('commute_upper'))
-        region = request.POST.get('region')
+    savePredict = PredictionInput(
+        income = income, 
+        children = children,
+        home_owner = home_owner,
+        cars = cars,
+        age = age,
+        occupation = occupation,
+        education = education,
+        marital_status = marital_status,
+        gender = gender,
+        commute_lower = commute_lower,
+        commute_upper = commute_upper,
+        region = region
+    )
+    savePredict.save()
 
-        user_input = PredictionInput.objects.create(
-            income=income,
-            children=children,
-            home_owner=home_owner,
-            cars=cars,
-            age=age,
-            occupation=occupation,
-            education=education,
-            marital_status=marital_status,
-            gender=gender,
-            commute_lower=commute_lower,
-            commute_upper=commute_upper,
-            region=region
-        )
-
-        input_vector = convert_to_model_input(user_input)
-        prediction = model.predict([input_vector])[0]
-
-    return render(request, 'index.html', {'prediction': prediction})
-
-def convert_to_model_input(data):
-    """
-    Convert a PredictionInput instance to a feature vector for the ML model.
-    This assumes one-hot encoding for occupation, education, and region.
-    Update this based on your model's training setup!
-    """
-    # One-hot mappings
-    occupation_map = {
-        'Clerical': [1, 0, 0, 0, 0],
-        'Management': [0, 1, 0, 0, 0],
-        'Manual': [0, 0, 1, 0, 0],
-        'Professional': [0, 0, 0, 1, 0],
-        'Skilled Manual': [0, 0, 0, 0, 1],
-    }
-
-    education_map = {
-        'Bachelors': [1, 0, 0, 0, 0],
-        'Graduate Degree': [0, 1, 0, 0, 0],
-        'High School': [0, 0, 1, 0, 0],
-        'Partial College': [0, 0, 0, 1, 0],
-        'Partial High School': [0, 0, 0, 0, 1],
-    }
-
-    region_map = {
-        'Europe': [1, 0, 0],
-        'North America': [0, 1, 0],
-        'Pacific': [0, 0, 1],
-    }
-
-    # Base numeric and boolean features
-    base_features = [
-        data.income,
-        data.children,
-        int(data.home_owner),
-        data.cars,
-        data.age,
-        int(data.marital_status),
-        int(data.gender),
-        data.commute_lower,
-        data.commute_upper,
-    ]
-
-    # Final input vector
-    return base_features + occupation_map[data.occupation] + education_map[data.education] + region_map[data.region]
-
+    return redirect('/polls/')
